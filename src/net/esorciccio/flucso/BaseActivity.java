9698b8aa466c938fb2a87c711c452556b9332d20
osiride <usiride@gmail.com>
2014-10-24 19:54:51 +0200

    requestWindowFeature -> supportRequestWindowFeature

--- src/net/esorciccio/flucso/BaseActivity.java
+++ src/net/esorciccio/flucso/BaseActivity.java
@@ -9,7 +9,7 @@ import android.os.Bundle;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
-//import android.view.Window;
+import android.view.Window;
 import android.widget.Toast;
 
 public abstract class BaseActivity extends ActionBarActivity {
@@ -22,7 +22,7 @@ public abstract class BaseActivity extends ActionBarActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		//requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
+		supportRequestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
 		
 		Log.v(logTag(), "onCreate");
 		
