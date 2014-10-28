cd276a5406b8ee97d7c92a102c7b09e66242afc0
osiride <usiride@gmail.com>
2014-10-28 17:59:54 +0100

    Toolbar for SettingsActivity

--- src/net/esorciccio/flucso/SettingsActivity.java
+++ src/net/esorciccio/flucso/SettingsActivity.java
@@ -1,15 +1,22 @@
 package net.esorciccio.flucso;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 
-public class SettingsActivity extends Activity {
+public class SettingsActivity extends ActionBarActivity {
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_settings);
 		
-		getFragmentManager().beginTransaction().replace(android.R.id.content, new SettingsFragment()).commit();
+		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+		if (toolbar != null) {
+            setSupportActionBar(toolbar);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+		
+		getFragmentManager().beginTransaction().replace(R.id.content_view, new SettingsFragment()).commit();
 	}
 }
